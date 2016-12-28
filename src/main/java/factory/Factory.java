c255e9792d6a32b458521d024b791a2e7e5b0ec3
Szymon Roman <szymon.roman@gmail.com>
2016-12-28 15:30:55 +0100

    Zawartosc klas zmiennych itd

--- src/main/java/factory/Factory.java
+++ src/main/java/factory/Factory.java
@@ -1,8 +1,27 @@
 package factory;
 
+import factory.methods.Method;
+import factory.variables.*;
+
 /**
  * Created by dziadziasmerf on 27.12.16.
  */
 public class Factory {
 
+    public Variable createVariable(String name, VariableType type) {
+        switch(type) {
+            case INT:
+                return new IntVariable(name);
+            case CHAR:
+                return new CharVariable(name);
+            case DOUBLE:
+                return  new DoubleVariable(name);
+            default:
+                return null;
+        }
+    }
+
+    public Method createMethod(String name) {
+        return new Method(name);
+    }
 }
--- src/main/java/factory/variables/Variable.java
+++ src/main/java/factory/variables/Variable.java
@@ -3,5 +3,11 @@ package factory.variables;
 /**
  * Created by dziadziasmerf on 28.12.16.
  */
-public interface Variable {
+public abstract class Variable {
+    public String name;
+
+    public Variable(String name) {
+        this.name = name;
+    }
+
 }
