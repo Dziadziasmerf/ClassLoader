262e3c477f172bafd1b88e466fc41fa39f69c6d1
Woy-tek <woy1157@gmail.com>
2016-12-30 17:56:00 +0100

    Update Method_Area.java

--- src/main/java/builder/elements/Method_Area.java
+++ src/main/java/builder/elements/Method_Area.java
@@ -6,6 +6,11 @@ public class Method_Area {
 	LinkedList<Variable> atributes;
 	LinkedList<Method> methods;
 	
+	public Method_Area(){
+		atributes = new LinkedList<Variable>();
+		methods = new LinkedList<Method>();
+	}
+	
 	void addAtr(Zmienna z){
 		atributes.add(z);
 	}
