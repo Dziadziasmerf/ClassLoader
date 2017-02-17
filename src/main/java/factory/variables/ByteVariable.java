0500572a08edd608289187a5fab1f7aa2b7cd654
Woy-tek <woy1157@gmail.com>
2017-02-18 00:43:04 +0100

    Create ByteVariable.java

---
+++ src/main/java/factory/variables/ByteVariable.java
@@ -0,0 +1,13 @@
+package factory.variables;
+
+public class ByteVariable extends Variable{
+	 public ByteVariable(String name) {
+	        super(name);
+	        this.type = VariableType.BYTE;
+	    }
+
+	    public ByteVariable(String name, Object value) {
+	        super(name);
+	        setValue(value);
+	    }
+}
