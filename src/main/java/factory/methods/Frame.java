a779606de397468720ee88386f404d9f388b9c72
Woy-tek <woy1157@gmail.com>
2017-02-18 00:38:37 +0100

    Create Frame.java

---
+++ src/main/java/factory/methods/Frame.java
@@ -0,0 +1,19 @@
+package factory.methods;
+
+/**
+ * Created by dziadziasmerf on 28.12.16.
+ */
+public class Frame {
+
+	Method method;
+	
+    public Frame(Method m) {
+        //TODO
+    	this.method = m;
+    }
+    
+    public Method getMethod(){
+    	return method;
+    }
+    
+}
