a623def473508b786db9f0b045cb17ff39cd95e9
Woy-tek <woy1157@gmail.com>
2017-02-18 00:40:24 +0100

    Create VariableType.java

---
+++ src/main/java/factory/variables/VariableType.java
@@ -0,0 +1,14 @@
+package factory.variables;
+
+/**
+ * Created by dziadziasmerf on 28.12.16.
+ */
+public enum VariableType {
+    INT(),
+    CHAR(),
+    DOUBLE(),
+    LONG(),
+    STRING(),
+    FLOAT(),
+    BYTE()
+}
