bf3c016d0d9359b9d3cdedd66e56c49081ae4682
Woy-tek <woy1157@gmail.com>
2017-02-18 00:42:17 +0100

    Create DoubleVariable.java

---
+++ src/main/java/factory/variables/DoubleVariable.java
@@ -0,0 +1,24 @@
+package factory.variables;
+
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
+
+/**
+ * Created by dziadziasmerf on 28.12.16.
+ */
+//@Getter
+//@Setter
+public class DoubleVariable extends Variable {
+
+    public DoubleVariable(String name) {
+        super(name);
+        this.type = VariableType.DOUBLE;
+    }
+
+    public DoubleVariable(String name, Object value) {
+        super(name);
+        setValue(value);
+    }
+
+}
