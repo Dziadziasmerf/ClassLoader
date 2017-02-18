1ba53172503490a45cd2cb674465d07e4a3fd4a4
Woy-tek <woy1157@gmail.com>
2017-02-18 15:21:40 +0100

    Update Builder.java

--- src/main/java/builder/building/Builder.java
+++ src/main/java/builder/building/Builder.java
@@ -27,13 +27,6 @@ public class Builder {
 		}
 		if (c == null) return false;
 		return true;
-		/*try{
-			Class<?> cl = Class.forName(name);
-			return true;
-		}catch(Exception e){
-			System.out.println("ERROR");
-			return false;
-		}*/
 	}
 	
 	public void buildClass(){
@@ -42,40 +35,13 @@ public class Builder {
 	}
 	
 	public void buildName(String s){
-		//Class c;
-		//c = Class.forName(s);
-		//Field f = new Field();
 		klasa.setName(s);
 	}
 	
 	public void buildAtribute(String name, VariableType type){
 		klasa.addAtr((Variable) factory.createVariable(name,type));
 	}
-
-/*	public void buildAtribute(String name, VariableType type, int value){
-		IntVariable v = (IntVariable) factory.createVariable(name,type);
-		v.setValue(value);
-		klasa.addAtrInt(v);
-	};
-	
-	public void buildAtribute(String name, VariableType type, char value){
-		CharVariable v = (CharVariable) factory.createVariable(name,type);
-		v.setValue(value);
-		klasa.addAtrChar(v);
-	};
-	
-	public void buildAtribute(String name, VariableType type, double value){
-		DoubleVariable v = (DoubleVariable) factory.createVariable(name,type);
-		v.setValue(value);
-		klasa.addAtrDouble(v);
-	};
-	*/
 	public void buildAtribute(String name, VariableType type, Object value){
-	/*	switch(type){
-			case INT: Variable v = factory.createVariable(name, type);
-			case DOUBLE: DoubleVariable v = (DoubleVariable) factory.createVariable(name, type);
-			case CHAR: CharVariable v = (CharVariable) factory.createVariable(name, type);
-		}*/
 		Variable v = (Variable) factory.createVariable(name,type);
 		v.setValue(value);
 		klasa.addAtr(v);
