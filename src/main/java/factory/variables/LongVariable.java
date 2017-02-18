8f2c6673c25de22b4b6029870b48b6f1f84d085f
Woy-tek <woy1157@gmail.com>
2017-02-18 14:44:49 +0100

    Update LongVariable.java

--- src/main/java/factory/variables/LongVariable.java
+++ src/main/java/factory/variables/LongVariable.java
@@ -9,5 +9,6 @@ public class LongVariable extends Variable{
 	    public LongVariable(String name, Object value) {
 	        super(name);
 	        setValue(value);
+	        this.type = VariableType.LONG;
 	    }
 }
