0ecdba43f1031146ef5da8e92b4210188901bbd2
Woy-tek <woy1157@gmail.com>
2017-02-18 15:27:44 +0100

    Update Method.java

--- src/main/java/factory/methods/Method.java
+++ src/main/java/factory/methods/Method.java
@@ -2,24 +2,13 @@ package factory.methods;
 
 import java.util.*;
 
-//import lombok.Getter;
-//import lombok.Setter;
-
-/**
- * Created by dziadziasmerf on 28.12.16.
- */
-
-//@Getter
-//@Setter
 public class Method {
 
     public String name;
-    //private Frame frame;
     private LinkedList<String> frame;
 
     public Method(String name, LinkedList<String> bytecode) {
         setName(name);
-        //setFrame(new Frame());
         setByteCode(bytecode);
     }
     
@@ -31,17 +20,10 @@ public class Method {
     	this.frame = b;
     }
     
-    //public void setFrame(Frame frame){
-    //	this.frame = frame;
-    //}
     
     public LinkedList<String> getFrame(){
     	return frame;
     }
-    
-    //public LinkedList<String> getByteCode(){
-    //	return frame;
-    //}
 
 
 }
